63edfdc422861f9595d585f87a8fd9e92ec367a9
Saila Karivieri <saila.karivieri@gmail.com>
2026-08-21 11:32:58 +0300

    ympyrä

---
+++ circle.py
@@ -0,0 +1,5 @@
+import math
+user_input = input("Enter the radius of the circle: ")
+radius = float(user_input)
+area = math.pi * radius ** 2
+print(f"The area of the circle with radius {radius} is: {area}")
